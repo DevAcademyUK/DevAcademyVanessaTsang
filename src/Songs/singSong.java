3fea56041910c18a68294f752fbabccc62699b4d
VanessaTsang888 <info@vanessatsang.uk>
2019-03-19 15:56:35 +0000

    inital commit

---
+++ src/Songs/singSong.java
@@ -0,0 +1,33 @@
+package Songs;
+
+import java.util.Scanner;
+
+public class singSong {
+    public static void main(String[]args){
+    singSong singing = new singSong();
+    singing.sing();
+    }
+
+    private void sing() {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println(" How many bottles of beer are there on the wall?");
+        int bottles = myScanner.nextInt();
+        String word = " bottles";
+        for (int i = bottles; i > 0; i--) {
+            System.out.println(i + "" + word + " of beer on the wall");
+            System.out.println(i + "" + word + " of beer");
+            System.out.println(" take one down");
+            System.out.println(" pass it around");
+            if (i == 1) {
+                System.out.println(" no more bottles of beer on the wall");
+            } else {
+                int newNum = i - 1;
+                if (newNum == 1) {
+                    word = "bottle";
+                }
+                System.out.println(newNum + "" + word + "of beer on the wall.");
+                System.out.println();
+            }
+        }
+    }
+}
