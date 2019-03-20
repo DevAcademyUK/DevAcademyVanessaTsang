e05448a56d69a9076a54cfed4df0fa40d57b9f39
VanessaTsang888 <info@vanessatsang.uk>
2019-03-20 13:02:31 +0000

    added game

--- src/Songs/FiveLittleDucks.java
+++ src/Songs/FiveLittleDucks.java
@@ -1,33 +1,34 @@
 package Songs;
 
 import java.util.Scanner;
+
 public class FiveLittleDucks {
-        public static void main(String[]args){
-            FiveLittleDucks song = new FiveLittleDucks();
-            song.ducks();
-        }
-        private void ducks() {
-            Scanner myScanner = new Scanner(System.in);
-            System.out.println(" Five little ducks");
-            int ducks = myScanner.nextInt();
-            String word = " ducks";
-            for (int i = ducks; i > 0; i--) {
-                System.out.println(i + "" + word + " went out one day");
-                System.out.println(i + "" + word + " over the hill and far away");
-                System.out.println(" mother duck said");
-                System.out.println(" Quark, quark, quark, quark");
-                if (i == 1) {
+    public static void main(String[] args) {
+        FiveLittleDucks song = new FiveLittleDucks();
+        song.duck();
+    }
+
+    private void duck() {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println(" Five little ducks");
+        int ducks = myScanner.nextInt();
+        String word = " ducks";
+        for (int i = ducks; i > 0; i--) {
+            System.out.println(i + "" + word + " went out one day");
+            System.out.println(i + "" + word + " went over the hill and far away");
+            System.out.println(" mother duck said");
+            System.out.println(" Quark, quark, quark, quark");
+            if (i == 1) {
+                word = " duck";
+            } else {
+                int newNum = i - 1;
+                if (newNum == 1) {
                     word = " duck";
                 }
-                else {
-                    int newNum = i - 1;
-                    if (newNum == 1){
-                        word = " ducks";
-                    }
-                    System.out.println(newNum + "" + word + "but only four ducks came back");
-                    System.out.println();
-                }
+                System.out.println(newNum + "" + word + "but only four ducks came back");
+                System.out.println();
             }
         }
     }
+}
 
